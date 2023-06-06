9a58ae36b9d51c682c3e0d1da1336e2deb25eb39
Victor Giovannoni Vernalha <victor.vernalha@mercadolivre.com>
2023-06-06 10:24:03 -0300

    Add API key middleware

--- webproject/cmd/server/main.go
+++ webproject/cmd/server/main.go
@@ -1,19 +1,34 @@
 package main
 
 import (
+	"os"
+
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"github.com/victorvernalha/go-web/webproject/cmd/server/handler"
+	"github.com/victorvernalha/go-web/webproject/cmd/server/middleware"
 	"github.com/victorvernalha/go-web/webproject/internal/transactions"
 )
 
+const API_KEY_NAME = "API_KEY"
+const API_KEY_HEADER = "authorization"
+
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		panic("Could not load .env file")
+	}
+
 	tRepo := transactions.CreateInMemoryRepo()
 	tService := transactions.DefaultService{Repo: &tRepo}
 	tHandler := handler.Transactions{Service: &tService}
 
+	tokenValidator := middleware.TokenValidator(os.Getenv(API_KEY_NAME), API_KEY_HEADER)
+
 	router := gin.Default()
 	group := router.Group("/transactions")
 	{
+		group.Use(tokenValidator)
 		group.GET("/", tHandler.GetAll())
 		group.POST("/", tHandler.Add())
 		group.PUT("/:id", tHandler.Replace())
---
+++ webproject/cmd/server/middleware/api_token.go
@@ -0,0 +1,26 @@
+package middleware
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func TokenValidator(token, header string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		values := c.Request.Header.Values(header)
+		if len(values) == 0 {
+			c.AbortWithError(http.StatusUnauthorized, fmt.Errorf("missing token in %s header", header))
+		}
+		if len(values) > 1 {
+			c.AbortWithError(http.StatusUnauthorized, fmt.Errorf("cannot have more than one value for %s header", header))
+		}
+		value := values[0]
+		fmt.Println(value, token)
+		if value != token {
+			c.AbortWithError(http.StatusUnauthorized, fmt.Errorf("invalid API key in header %s", header))
+		}
+		c.Next()
+	}
+}
