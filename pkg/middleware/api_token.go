43cce541728f8104af4afff3fa6e0f75592a0f59
Victor Giovannoni Vernalha <victor.vernalha@mercadolivre.com>
2023-06-06 12:49:54 -0300

    Add missing return statements in token middleware

--- pkg/middleware/api_token.go
+++ pkg/middleware/api_token.go
@@ -12,14 +12,17 @@ func TokenValidator(token, header string) gin.HandlerFunc {
 		values := c.Request.Header.Values(header)
 		if len(values) == 0 {
 			c.AbortWithError(http.StatusUnauthorized, fmt.Errorf("missing token in %s header", header))
+			return
 		}
 		if len(values) > 1 {
 			c.AbortWithError(http.StatusUnauthorized, fmt.Errorf("cannot have more than one value for %s header", header))
+			return
 		}
 		value := values[0]
 		fmt.Println(value, token)
 		if value != token {
 			c.AbortWithError(http.StatusUnauthorized, fmt.Errorf("invalid API key in header %s", header))
+			return
 		}
 		c.Next()
 	}
